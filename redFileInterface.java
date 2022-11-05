05f6f9b44ead982b94527002281f6dcb1710664a
jaivikppatel <jaivikppatel@gmail.com>
2022-11-05 15:03:02 -0400

    Added endOfFile and close

--- redFileInterface.java
+++ redFileInterface.java
@@ -15,6 +15,7 @@ public class redFileInterface {
     private Boolean get_flag, put_flag;
     private PrintWriter out;
 
+    // initialize with red filename
     redFileInterface(String filename) {
         this.filename = filename;
         this.get_flag = false;
@@ -29,6 +30,7 @@ public class redFileInterface {
         this.red_file = new File(this.filename);
     }
 
+    // gets next int with -32 -- used during encryption
     protected int getNextInt() throws FileNotFoundException {
         if (!this.get_flag) {
             this.get_flag = true;
@@ -36,17 +38,37 @@ public class redFileInterface {
             scan.useDelimiter("");
         }
         if (scan.hasNext())
-            return (int) scan.next().charAt(0);
+            return ((int) scan.next().charAt(0) - 32);
         else
             this.get_flag = false;
         return -1;
     }
 
+    // put next int with +32 -- used during decryption
     protected void putNextChar(int x) throws IOException {
         if (!this.put_flag) {
             this.put_flag = true;
             out = new PrintWriter(new BufferedWriter(new FileWriter("test.txt", true)));
         }
         out.print((char) (x + 32));
+    }
+
+    protected void endOfFile(){
+        System.out.println("RedFile End Reached");
+        //send audit message here
+        System.out.println("The Audit message has been sent");
+    }
+
+    protected boolean close(){
+        try {
+            this.filename = null;
+            this.red_file = null;
+            this.get_flag = false;
+            this.put_flag = false;
+            return false;
+        } catch (Exception e) {
+            System.out.println("Failed to close the Interface");
+            throw new RuntimeException(e);
+        }
     }
 }
