0353a9ae455fbfe6c98ac72415aa89a9dee12860
jaivikppatel <jaivikppatel@gmail.com>
2022-11-18 00:42:21 -0500

    Updated Interfaces

--- blackFileInterface.java
+++ blackFileInterface.java
@@ -1,29 +1,40 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class blackFileInterface {
     private final String filename;
+    private final Opt opt;
     private BufferedReader reader;
     private PrintWriter printer;
-    private boolean EOF_flag;
+    private int itr;
+    private ArrayList<Integer> int_list = new ArrayList<>();
 
-    blackFileInterface(String filename){
+    blackFileInterface(String filename, Opt opt){
         this.filename = filename;
+        this.opt = opt;
+        this.itr = 0;
     }
 
-    public static blackFileInterface Instance(String filename) { return new blackFileInterface(filename); }
+    public static blackFileInterface Instance(String filename, Opt opt) { return new blackFileInterface(filename, opt); }
 
     protected void openFile() throws IOException {
-        this.reader = new BufferedReader(new FileReader(this.filename));
-        this.printer = new PrintWriter(new BufferedWriter(new FileWriter(this.filename, true)));
-        this.EOF_flag = false;
+        if (this.opt == Opt.ENCRYPT){
+            int temp;
+            this.reader = new BufferedReader(new FileReader(this.filename));
+            while ((temp = Integer.parseInt(reader.readLine())) != -1){
+                int_list.add(temp);
+            }
+            Collections.reverse(int_list);
+        }
+        if (this.opt == Opt.DECRYPT)
+            this.printer = new PrintWriter(new BufferedWriter(new FileWriter(this.filename, true)));
     }
 
     protected int getNextInt() throws IOException {
-        int ret = Integer.parseInt(reader.readLine());
-        if (ret < 0){
-            this.EOF_flag = true;
-        }
+        int ret = int_list.get(itr);
+        itr++;
         return ret;
     }
 
@@ -33,7 +44,7 @@ public class blackFileInterface {
 
     // check if EOF
     protected boolean endOfFile(){
-        return this.EOF_flag;
+        return (itr >= int_list.size());
     }
 
     // closes the instance
--- redFileInterface.java
+++ redFileInterface.java
@@ -1,6 +1,5 @@
-
 import java.io.*;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class redFileInterface {
 
@@ -9,12 +8,14 @@ public class redFileInterface {
     private final Opt opt;
     private BufferedReader reader;
     private PrintWriter printer;
-    private boolean EOF_flag;
+    private ArrayList<Integer> int_list = new ArrayList<>();
+    private int itr;
 
     // initialize with red filename
     redFileInterface(String filename, Opt opt) {
         this.filename = filename;
         this.opt = opt;
+        this.itr = 0;
     }
 
     public static redFileInterface Instance(String filename, Opt opt) {
@@ -22,19 +23,21 @@ public class redFileInterface {
     }
 
     protected void openFile() throws IOException {
-        if (this.opt == Opt.ENCRYPT)
+        if (this.opt == Opt.ENCRYPT) {
+            int temp;
             this.reader = new BufferedReader(new FileReader(this.filename));
+            while ((temp = reader.read()) != -1){
+                int_list.add(temp);
+            }
+        }
         if (this.opt == Opt.DECRYPT)
             this.printer = new PrintWriter(new BufferedWriter(new FileWriter(this.filename, true)));
-        this.EOF_flag = false;
     }
 
     // gets next int with -32 -- used during encryption
     protected int getNextInt() throws IOException {
-        int ret = reader.read() - 32;
-        if (ret < 0){
-            this.EOF_flag = true;
-        }
+        int ret = int_list.get(itr);
+        itr++;
         return ret;
     }
 
@@ -45,7 +48,7 @@ public class redFileInterface {
 
     // checks for EOF
     protected boolean endOfFile(){
-        return this.EOF_flag;
+        return (itr >= int_list.size());
     }
 
     // closes the instance
